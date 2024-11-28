c80527c4c2c5ed75f77307ef9f382708646d1182
Frederick Obeng-Nyarko <nernuer@gmail.com>
2024-11-28 15:59:11 +0000

    chore: finish moving existing code to plain react

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import MoviesList from './pages/home.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import MovieDetail from './pages/details.tsx'
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// export const links = () => [
+//   { rel: "preconnect", href: "https://fonts.googleapis.com" },
+//   {
+//     rel: "preconnect",
+//     href: "https://fonts.gstatic.com",
+//     crossOrigin: "anonymous",
+//   },
+//   {
+//     rel: "stylesheet",
+//     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+//   },
+//   // { rel: "stylesheet", href: stylesheet },
+// ];
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/" element={<MoviesList />} />
+          <Route path="/movie/:id" element={<MovieDetail />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 )
---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          '"Inter"',
+          'ui-sans-serif',
+          'system-ui',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
